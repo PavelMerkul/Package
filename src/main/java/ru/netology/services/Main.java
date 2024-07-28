0cde6e23b0f81fd707095484850a7d0ed9ea8750
Pavel MSK <sonev@bk.ru>
2024-07-28 12:48:29 +0300

    commit test5

--- src/main/java/ru/netology/services/Main.java
+++ src/main/java/ru/netology/services/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         VacationService service = new VacationService();
 
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
+        int income = 10_000; // доход в месяц
+        int expenses = 3_000; // расход в месяц
+        int threshold = 20_000; // ограничение
 
         int vacationMonths = service.calculateVacationMonths(income, expenses, threshold);
 
